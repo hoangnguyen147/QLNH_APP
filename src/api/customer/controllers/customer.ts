25d4b03845c821161c71956ad03fe755d4097497
hoangnguyen147 <huyhoangnguyen147z@gmail.com>
2023-06-12 20:47:17 +0700

    ver 1.0.2

---
+++ src/api/customer/controllers/customer.ts
@@ -0,0 +1,7 @@
+/**
+ * customer controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::customer.customer');
---
+++ src/api/customer/routes/customer.ts
@@ -0,0 +1,7 @@
+/**
+ * customer router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::customer.customer');
